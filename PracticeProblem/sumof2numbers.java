0c089326dd863cc85bc057178a2797338582ef53
harsh201004 <harshraghuwanshi2010@gmail.com>
2025-01-08 10:39:44 +0530

    Sum of two numbers

--- PracticeProblem/sumof2numbers.java
+++ PracticeProblem/sumof2numbers.java
@@ -1,33 +1,5 @@
-// import java.util.Scanner;
-
-// public class sumof2numbers {
-//   public static void main(String[] args) 
-//   {
-//     Scanner input = new Scanner(System.in);
-//     System.out.print(" First number: ");
-//     int num1 = input.nextInt();
-//     System.out.print(" Second number: ");
-//     int num2 = input.nextInt();
-//     int sum = num1 + num2;
-//     System.out.println();
-    
-//     System.out.println("Sum: " + sum);
-//   }
-// }
-
-import java.util.scanner;
-
-public class celsiustofahrenheit{
-   public static void main(String [] args);{
-    
-   Scanner input = new input(System.in);
-
-      System.out.println("Enter the temperature in celsius: ");
-
-      int Celsius = input.nextInt();
-      
-      int Fahrenheit = (Celsius * 9/5) + 32;
-
-      System.out.println("Temperature in Fahrenheit is: " + Fahrenheit);
+public class printbridgelabz {
+public static void main(String [] args){
+ System.out.println("Welcome to Bridgelabz!");
 }
 }
